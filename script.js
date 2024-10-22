c772c128a36a8869df777d92ecea78bd474a7e63
VictorioIND <victorio.ind@gmail.com>
2024-10-21 23:03:12 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    const parallax = document.querySelectorAll('.card');
-    parallax.forEach(card => {
-        let offset = window.pageYOffset;
-        card.style.backgroundPositionY = (offset * 0.5) + 'px';
-    });
-});
